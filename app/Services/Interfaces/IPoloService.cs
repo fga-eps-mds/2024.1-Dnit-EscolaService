a915e685efc999a2add3f6ea0a9408e972b64e9a
Artur Bartz <arturbylly2@gmail.com>
2023-12-02 15:02:07 -0300

    feat: adiciona as funções de Update e Delete do CRUD de gerenciar polos no arquivo IPoloService.cs

--- app/Services/Interfaces/IPoloService.cs
+++ app/Services/Interfaces/IPoloService.cs
@@ -9,4 +9,6 @@ public interface IPoloService
     Task<Polo> ObterPorIdAsync(int id);
     Task CadastrarAsync(CadastroPoloDTO poloDto);
     Task<ListaPaginada<PoloModel>> ListarPaginadaAsync(PesquisaPoloFiltro filtro);
+    Task AtualizarAsync(Polo poloExistente, CadastroPoloDTO poloDto);
+    Task ExcluirAsync(Polo poloExistente);
 }
