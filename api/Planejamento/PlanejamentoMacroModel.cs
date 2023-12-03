79d185bcb2a69794c0ca11d90a0e6391a00af8a9
Daniel Porto <danielportods@gmail.com>
2023-12-03 04:35:02 -0300

    feat: cria classes model para o planejamento macro

---
+++ api/Planejamento/PlanejamentoMacroModel.cs
@@ -0,0 +1,26 @@
+namespace api.Planejamento
+{
+    public class PlanejamentoMacroModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Responsavel { get; set; }
+        public Mes MesInicio { get; set; }
+        public Mes MesFim { get; set; }    
+        public string AnoInicio { get; set; }
+        public string AnoFim { get; set; }
+        public int QuantidadeAcoes { get; set; }
+    }
+
+    public class PlanejamentoMacroDetralhadoModel : PlanejamentoMacroModel
+    {
+        public List<DetalhesPorUF> DetalhesPorUF { get; set; }
+        public List<PlanejamentoMacroMensalModel> PlanejamentoMacroMensal { get; set; }
+    }
+
+    public class DetalhesPorUF
+    {
+        public UF UF { get; set; }
+        public int QuantidadeEscolasTotal { get; set; }
+    }
+}
