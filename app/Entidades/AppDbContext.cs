8b28062a1861cf48fb702d53e5a6c37efbbdfaed
Daniel Porto <danielportods@gmail.com>
2023-12-03 01:31:51 -0300

    feat: cria entidades de PlanejamentoMacro

--- app/Entidades/AppDbContext.cs
+++ app/Entidades/AppDbContext.cs
@@ -12,6 +12,8 @@ namespace app.Entidades
         public DbSet<Ranque> Ranques { get; set; }
         public DbSet<EscolaRanque> EscolaRanques { get; set; }
         public DbSet<Superintendencia> Superintendencias { get; set; }
+        public DbSet<PlanejamentoMacro> PlanejamentoMacro { get; set; }
+        public DbSet<PlanejamentoMacroEscola> PlanejamentoMacroEscola {get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
@@ -21,11 +23,21 @@ namespace app.Entidades
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Escola>().HasMany(escola => escola.EtapasEnsino).WithOne(e => e.Escola);
+            modelBuilder.Entity<Escola>()
+                .HasMany(escola => escola.EtapasEnsino)
+                .WithOne(e => e.Escola);
             modelBuilder.Entity<Ranque>()
-                .Property(r => r.Id).ValueGeneratedOnAdd();
+                .Property(r => r.Id)
+                .ValueGeneratedOnAdd();
             modelBuilder.Entity<EscolaRanque>()
-                .Property(r => r.Id).ValueGeneratedOnAdd();
+                .Property(r => r.Id)
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<PlanejamentoMacro>()
+                .HasMany(planejamentoMacro => planejamentoMacro.Escolas)
+                .WithOne(escolas => escolas.PlanejamentoMacro);
+            modelBuilder.Entity<PlanejamentoMacroEscola>()
+                .HasOne(PlanejamentoMacroEscola => PlanejamentoMacroEscola.Escola)
+                .WithMany();
         }
 
         public void Popula()
---
+++ app/Entidades/PlanejamentoMacroEscola.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using api;
+
+namespace app.Entidades
+{
+    public class PlanejamentoMacroEscola
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Mes Mes { get; set; }
+
+        [Required]
+        public string Ano { get; set; }
+
+        [Required]
+        public Guid PlanejamentoMacroId { get; set; }
+
+        public PlanejamentoMacro PlanejamentoMacro { get; set; }
+
+        [Required]
+        public Guid EscolaId { get; set; }
+
+        public Escola Escola { get; set; }
+    }
+}
