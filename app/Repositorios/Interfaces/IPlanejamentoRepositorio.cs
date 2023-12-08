aad76b2c19b11e90e46a5b6c376fafc5ec1cc61c
bottinolucas <lucasgabottino@gmail.com>
2023-12-08 00:01:49 -0300

    feat: definindo camada repositório para implementar o delete
    Co-authored-by: Daniel Porto <danielportods@gmail.com>
    Co-authored-by: Vitor Lamego <vitorlamegoo@gmail.com>
    Co-authored-by: João G. Carvalho <joaoseisei@gmail.com>

--- app/Repositorios/Interfaces/IPlanejamentoRepositorio.cs
+++ app/Repositorios/Interfaces/IPlanejamentoRepositorio.cs
@@ -6,7 +6,9 @@ namespace app.Repositorios.Interfaces
     public interface IPlanejamentoRepositorio
     {
         //Definir os metodos a serem implementados
-        Task<PlanejamentoMacroDetalhadoModel> ObterPlanejamentoMacroDetalhado(Guid id);
+        void ExcluirPlanejamentoMacro(PlanejamentoMacro pm);
+        void ExcluirPlanejamentoMacroEscola(PlanejamentoMacroEscola pm);
+        Task<PlanejamentoMacro> ObterPlanejamentoMacroAsync(Guid id);
         PlanejamentoMacro RegistrarPlanejamentoMacro(PlanejamentoMacro pm);
         Task<List<PlanejamentoMacro>> ListarAsync();
     }
