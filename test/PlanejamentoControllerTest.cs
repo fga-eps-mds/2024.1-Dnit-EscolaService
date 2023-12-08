25bfc0de5eadde74630c2f85b9533c4a35b4d00b
João Antonio Ginuino Carvalho <joaoseisei@gmail.com>
2023-12-08 15:03:43 -0300

    feat: Adiciona classe de testes do PlanejamentoController
    Co-authored-by: Lucas Bottino <lucasgabottino@gmail.com>

---
+++ test/PlanejamentoControllerTest.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using api.Escolas;
+using app.Controllers;
+using app.Entidades;
+using app.Services;
+using System.Threading.Tasks;
+using api.Polos;
+using auth;
+using Microsoft.EntityFrameworkCore;
+using test.Fixtures;
+using test.Stubs;
+using Xunit.Abstractions;
+
+namespace test
+{
+    public class PlanejamentoControllerTest : AuthTest, IDisposable
+    {
+        private readonly PlanejamentoController controller;
+        private readonly AppDbContext dbContext;
+
+        public PlanejamentoControllerTest(ITestOutputHelper testOutputHelper, Base fixture) : base(testOutputHelper, fixture)
+        {
+            dbContext = fixture.GetService<AppDbContext>(testOutputHelper)!;
+            controller = fixture.GetService<PlanejamentoController>(testOutputHelper)!;
+            AutenticarUsuario(controller);
+            // dbContext.PlanejamentoMacro(5);        
+        }
+    }
+}
