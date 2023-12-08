b75e94de072ce35e63845e3b06d398253f293050
João Antonio Ginuino Carvalho <joaoseisei@gmail.com>
2023-12-08 15:01:35 -0300

    feat: Adicona ListarPlanejamentoMacro no stub
    Co-authored-by: Lucas Bottino <lucasgabottino@gmail.com>

--- test/Stubs/PlanejamentoMacroStub.cs
+++ test/Stubs/PlanejamentoMacroStub.cs
@@ -8,6 +8,28 @@ namespace test.Stubs
 {
     public static class PlanejamentoMacroStub
     {
+        public static IEnumerable<PlanejamentoMacro> ListarPlanejamentoMacro(IEnumerable<Municipio> municipios)
+        {
+            while (true)
+            {
+                var planejamentoMacro = new PlanejamentoMacro
+                {
+                    Id = Guid.NewGuid(),
+                    Nome = $"Planejamento {Random.Shared.Next() % 10} - {Random.Shared.Next()}",
+                    Responsavel = $"Joao - {Random.Shared.Next().ToString()}",
+                    MesInicio = Mes.Abril,
+                    MesFim = Mes.Julho,
+                    AnoInicio = "2023",
+                    AnoFim = "2023",
+                    QuantidadeAcoes = Random.Shared.Next() % 10,
+                };
+
+                var planejamentoMacroEscola = ListarPlanejamentoMacroEscolas(municipios, planejamentoMacro).Take(Random.Shared.Next(0,5));
+                planejamentoMacro.Escolas = planejamentoMacroEscola.ToList();
+
+                yield return planejamentoMacro;
+            }
+        }
 
         public static IEnumerable<PlanejamentoMacroEscola> ListarPlanejamentoMacroEscolas(IEnumerable<Municipio> municipios, PlanejamentoMacro planejamentoMacro)
         {
